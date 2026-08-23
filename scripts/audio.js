eee528bdacd5b68a64f07c4b785129e5a384d045
Laet441 <lightdeveloperru@gmail.com>
2026-08-24 02:52:04 +0400

    update audio.js

--- scripts/audio.js
+++ scripts/audio.js
@@ -2,10 +2,11 @@ const audio = {
   play(fileName, isMusic) {
     const path = "../assets/audio/" + (isMusic ? "music" : "sounds") + "/" + fileName;
     
-    console.log("Audio path: " + path);
+    //console.log("Audio path: " + path);
     
     const aud = new Audio(path);
     aud.volume = 0.2;
+    aud.loop = true;
     aud.play();
     
   }
